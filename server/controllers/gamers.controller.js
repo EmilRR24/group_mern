efb9c6ab64fed9c4eda17621ccc6c4b622312266
KYU102 <kevinyu25@gmail.com>
2022-05-23 22:09:48 -0700

    completed registration page and backend

--- server/controllers/gamers.controller.js
+++ server/controllers/gamers.controller.js
@@ -1,4 +1,6 @@
-const Gamer = require("../models/gamer.model")
+const {Gamer} = require("../models/gamer.model")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 
 // TEST
 module.exports.test = (req,res) => {
@@ -41,3 +43,49 @@ module.exports.deleteGamer = (req, res) => {
         .then(result => res.json({ result }))
         .catch(err => res.json(err));
 };
+
+//-----------LOGIN/REGISTRATION AUTHENTICATION---------------------------//
+module.exports.register = (req, res) => {
+    Gamer.exists({email : req.body.email})
+        .then(gamerExists => {
+            // IS GAMER EXISTS, SEND BACK AN ERROR
+            if(gamerExists){
+                return Promise.reject({
+                    errors: {"duplicate": "Email already taken"}
+                })
+            }
+            else{
+                const gamer = new Gamer(req.body)
+                return gamer.save()
+            }
+        })
+        .then(gamer => res.json(gamer))
+        .catch(err => res.status(400).json(err))
+}
+
+
+module.exports.login = (req,res) => {
+    // CHECK TO SEE IF THE EMAIL EXISTS
+    Gamer.findOne({email : req.body.email})
+        .then(gamer => {
+            if(gamer === null){
+                res.status(400).json({msg: "INVALID LOGIN"})
+            }
+            else{
+                bcrypt.compare(req.body.password, gamer.password)
+                    .then(passwordIsValid => {
+                        if(passwordIsValid){
+                            const newJWT = jwt.sign({
+                                _id:gamer._id
+                            }, "SECRET_KEY")
+                            res.cookie("gamertoken", newJWT, {httpOnly:true}).json("success")
+                        }
+                        else{
+                            res.status(400).json({msg:"INVALID ATTEMPT"})
+                        }
+                    })
+            }
+        })
+
+}
+//-----------LOGIN/REGISTRATION AUTHENTICATION---------------------------//
